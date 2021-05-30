375fad1c27fa98973fcfa6e26541af8a406b3b2d
Weihang Lo <me@weihanglo.tw>
2021-05-30 12:35:57 +0800

    feat: use external blob from sqlite

--- src/cargo/sources/registry/db.rs
+++ src/cargo/sources/registry/db.rs
@@ -1,6 +1,8 @@
 use crate::CargoResult;
 use once_cell::sync::OnceCell;
-use rusqlite::{params, Connection};
+use rusqlite::blob::{Blob, ZeroBlob};
+use rusqlite::{params, Connection, DatabaseName};
+use std::io::{Read, Write};
 use std::path::Path;
 use std::sync::Mutex;
 
@@ -35,11 +37,17 @@ impl Db {
         K: AsRef<[u8]>,
     {
         let key = key.as_ref();
-        Ok(self.0.query_row(
-            "SELECT contents FROM summaries WHERE name = ? LIMIT 1",
+        let row_id = self.0.query_row(
+            "SELECT rowid FROM summaries WHERE name = ? LIMIT 1",
             [key],
             |row| row.get(0),
-        )?)
+        )?;
+        let mut blob = self.blob_open(row_id, false)?;
+        let len = blob.len();
+        let mut buf = Vec::with_capacity(len);
+        let bytes_read = blob.read_to_end(&mut buf)?;
+        assert_eq!(bytes_read, len);
+        Ok(buf)
     }
 
     pub fn insert<K>(&self, key: K, value: &[u8]) -> CargoResult<()>
@@ -47,12 +55,27 @@ impl Db {
         K: AsRef<[u8]>,
     {
         let key = key.as_ref();
-        let modified = self.0.execute(INSERT_SUMMERIES, params![key, value])?;
+        let zblob = ZeroBlob(value.len() as i32);
+        let modified = self.0.execute(INSERT_SUMMERIES, params![key, zblob])?;
+        let row_id = self.0.last_insert_rowid();
+        let mut blob = self.blob_open(row_id, false)?;
+        let bytes_written = blob.write(value)?;
+        assert_eq!(bytes_written, value.len());
         log::debug!(
             "insert {} record for {}",
             modified,
             String::from_utf8_lossy(key)
         );
         Ok(())
+    }
+
+    fn blob_open(&self, row_id: i64, read_only: bool) -> CargoResult<Blob<'_>> {
+        Ok(self.0.blob_open(
+            DatabaseName::Main,
+            "summaries",
+            "contents",
+            row_id,
+            read_only,
+        )?)
     }
 }
